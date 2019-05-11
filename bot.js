aacbf5e44c38ec7070a49ef743a58d5707f809d7
hamo2111 <50474485+hamo2111@users.noreply.github.com>
2019-05-11 05:15:16 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`انا زوجة حامو`,"http://twitch.tv/Hamo")
+client.user.setGame(`الله اما اني صايم`,"http://twitch.tv/Hamo")
 client.user.setStatus("dnd")
 });
 
